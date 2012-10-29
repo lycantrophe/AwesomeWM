0923cf61a84ac3e496c4b8cf08c914ee9c143994
Yggdrasill <yggdrasill@lavabit.com>
2012-10-29 20:09:05 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,1 @@
+AwesomeWM configuration files go here.
---
+++ theme/muted-dream-red/theme.lua
@@ -0,0 +1,71 @@
+-- muted-dream-tree, awesome3 theme, by him
+
+--{{{ Main
+
+theme = {}
+
+theme.font          = "Terminus 9"
+
+theme.useless_gap_width  = "3"
+
+theme.bg_normal     = "#000000"
+theme.bg_focus      = "#000000"
+theme.bg_urgent     = "#000000"
+theme.bg_minimize   = "#000000"
+
+theme.fg_focus      = "#B23308"
+theme.fg_normal     = "#ECDDA6"
+theme.fg_urgent     = "#AA5530"
+theme.fg_minimize   = "#333333"
+
+theme.border_width  = "0"
+theme.border_normal = "#000000"
+theme.border_focus  = "#000000"
+theme.border_marked = "#000000"
+
+theme.taglist_squares_sel = sharedthemes .. "/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = sharedthemes .. "/default/taglist/squarew.png"
+
+theme.tasklist_floating_icon = sharedthemes .. "/default/tasklist/floatingw.png"
+
+theme.menu_submenu_icon = sharedthemes .. "/default/submenu.png"
+theme.menu_height = "15"
+theme.menu_width = "100"
+
+theme.titlebar_close_button_normal = sharedthemes .. "/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus = sharedthemes .. "/default/titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = sharedthemes .. "/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = sharedthemes .. "/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = sharedthemes .. "/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = sharedthemes .. "/default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = sharedthemes .. "/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = sharedthemes .. "/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = sharedthemes .. "/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = sharedthemes .. "/default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = sharedthemes .. "/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = sharedthemes .. "/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = sharedthemes .. "/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = sharedthemes .. "/default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = sharedthemes .. "/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = sharedthemes .. "/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = sharedthemes .. "/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = sharedthemes .. "/default/titlebar/maximized_focus_active.png"
+
+theme.layout_fairh = sharedthemes .. "/default/layouts/fairhw.png"
+theme.layout_fairv = sharedthemes .. "/default/layouts/fairvw.png"
+theme.layout_floating = sharedthemes .. "/default/layouts/floatingw.png"
+theme.layout_magnifier = sharedthemes .. "/default/layouts/magnifierw.png"
+theme.layout_max = sharedthemes .. "/default/layouts/maxw.png"
+theme.layout_fullscreen = sharedthemes .. "/default/layouts/fullscreenw.png"
+theme.layout_tilebottom = sharedthemes .. "/default/layouts/tilebottomw.png"
+theme.layout_tileleft = sharedthemes .. "/default/layouts/tileleftw.png"
+theme.layout_tile = sharedthemes .. "/default/layouts/tilew.png"
+theme.layout_tiletop = sharedthemes .. "/default/layouts/tiletopw.png"
+theme.layout_spiral = sharedthemes .. "/default/layouts/spiralw.png"
+theme.layout_dwindle = sharedthemes .. "/default/layouts/dwindlew.png"
+
+return theme
